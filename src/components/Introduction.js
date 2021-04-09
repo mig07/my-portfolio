603724bf97e91c374d67227374a13c0de839724d
Miguel <miguelfluis7@gmail.com>
2021-04-09 15:43:58 +0100

    Finished intro and commented particles js code (cpu + gpu heavy)

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,37 +4,38 @@ import Particles from 'react-particles-js';
 
 export default function Introduction() {
     return (
-
-        <div className="wrapper">
-            <div className='header-container'>
+        <div className="header-container">
+            <div className="header-text-container">
                 <h1 className='header-title'>Hello World!</h1>
                 <h1 className='header-title'>I'm Miguel Luís</h1>
                 <h3 className='header-subtitle'>I'm a software engineer and a MSc computer engineering student</h3>
                 <h4 className='header-subtitle'>Welcome to my web portfolio!</h4>
             </div>
-            <Particles
-                params={{
-                    "particles": {
-                        "number": {
-                            "value": 50
-                        },
-                        "size": {
-                            "value": 7
-                        },
-                        "interactivity": {
-                            "events": {
-                                "onhover": {
-                                    "enable": true,
-                                    "mode": "repulse"
+            {/* <div className="wrapper">
+                <Particles
+                    params={{
+                        "particles": {
+                            "number": {
+                                "value": 50
+                            },
+                            "size": {
+                                "value": 7
+                            },
+                            "interactivity": {
+                                "events": {
+                                    "onhover": {
+                                        "enable": true,
+                                        "mode": "repulse"
+                                    }
                                 }
+                            },
+                            "links": {
+                                "color": "#000000"
                             }
                         },
-                        "links": {
-                            "color": "#000000"
-                        }
-                    },
-                }}                
-            />
+                    }}                
+                />
+            </div> */}
         </div>
     )
 }
